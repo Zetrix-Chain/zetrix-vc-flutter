411ad0a00a736f389902c6aa8858d4c6319cbcfe
Ammar <armmarov@gmail.com>
2025-07-02 10:47:57 +0800

    Fix for ios

--- ios/Classes/BbsFlutterPlugin.m
+++ ios/Classes/BbsFlutterPlugin.m
@@ -9,8 +9,8 @@
 @property (nonatomic, strong) NSData *blinding_factor;
 
 - (instancetype)initWithType:(int)type
-                    message:(NSData *)message
-                   blinding:(NSData *)blinding_factor;
+                     message:(NSData *)message
+                    blinding:(NSData *)blinding_factor;
 
 @end
 
@@ -18,8 +18,8 @@
 @implementation ProofMessage
 
 - (instancetype)initWithType:(int)type
-                    message:(NSData *)message
-                   blinding:(NSData *)blinding_factor {
+                     message:(NSData *)message
+                    blinding:(NSData *)blinding_factor {
     self = [super init];
     if (self) {
         _type = type;
@@ -57,96 +57,96 @@ static const int PROOF_MESSAGE_TYPE_HIDDEN_EXTERNAL_BLINDING = 3;
 // Add after your existing code but before @end
 
 - (NSData *)blsCreateProof:(NSData *)publicKey
-                    nonce:(NSData *)nonce
-               signature:(NSData *)signature
-                messages:(NSArray *)messages {
+                     nonce:(NSData *)nonce
+                 signature:(NSData *)signature
+                  messages:(NSArray *)messages {
     bbs_signature_error_t error;
-    
+
     // Convert public key to BBS public key
     bbs_signature_byte_buffer_t bbsPublicKeyBuffer;
     bbs_signature_byte_buffer_t inputPubKey = {
-        .data = (uint8_t *)publicKey.bytes,
-        .len = publicKey.length
+            .data = (uint8_t *)publicKey.bytes,
+            .len = publicKey.length
     };
-    
+
     if (bls_public_key_to_bbs_key(inputPubKey, (uint32_t)messages.count, &bbsPublicKeyBuffer, &error) != 0) {
         NSString *errorMsg = error.message ? @(error.message) : @"Unable to convert public key";
         @throw [NSException exceptionWithName:@"BBSError" reason:errorMsg userInfo:nil];
     }
-    
+
     // Initialize proof context
     uint64_t handle = bbs_create_proof_context_init(&error);
     if (handle == 0) {
         @throw [NSException exceptionWithName:@"BBSError"
-                                    reason:@"Unable to create proof context"
-                                  userInfo:nil];
+                                       reason:@"Unable to create proof context"
+                                     userInfo:nil];
     }
-    
+
     // Set public key
     if (bbs_create_proof_context_set_public_key(handle, bbsPublicKeyBuffer, &error) != 0) {
         bbs_byte_buffer_free(bbsPublicKeyBuffer);
         @throw [NSException exceptionWithName:@"BBSError"
-                                    reason:@"Unable to set public key"
-                                  userInfo:nil];
+                                       reason:@"Unable to set public key"
+                                     userInfo:nil];
     }
-    
+
     // Set nonce
     bbs_signature_byte_buffer_t nonceBuffer = {
-        .data = (uint8_t *)nonce.bytes,
-        .len = nonce.length
+            .data = (uint8_t *)nonce.bytes,
+            .len = nonce.length
     };
     if (bbs_create_proof_context_set_nonce_bytes(handle, nonceBuffer, &error) != 0) {
         bbs_byte_buffer_free(bbsPublicKeyBuffer);
         @throw [NSException exceptionWithName:@"BBSError"
-                                    reason:@"Unable to set nonce"
-                                  userInfo:nil];
+                                       reason:@"Unable to set nonce"
+                                     userInfo:nil];
     }
-    
+
     // Set signature
     bbs_signature_byte_buffer_t sigBuffer = {
-        .data = (uint8_t *)signature.bytes,
-        .len = signature.length
+            .data = (uint8_t *)signature.bytes,
+            .len = signature.length
     };
     if (bbs_create_proof_context_set_signature(handle, sigBuffer, &error) != 0) {
         bbs_byte_buffer_free(bbsPublicKeyBuffer);
         @throw [NSException exceptionWithName:@"BBSError"
-                                    reason:@"Unable to set signature"
-                                  userInfo:nil];
+                                       reason:@"Unable to set signature"
+                                     userInfo:nil];
     }
-    
+
     // Add messages
     for (ProofMessage *msg in messages) {
         bbs_signature_byte_buffer_t msgBuffer = {
-            .data = (uint8_t *)msg.message.bytes,
-            .len = msg.message.length
+                .data = (uint8_t *)msg.message.bytes,
+                .len = msg.message.length
         };
         bbs_signature_byte_buffer_t blindBuffer = {
-            .data = (uint8_t *)msg.blinding_factor.bytes,
-            .len = msg.blinding_factor.length
+                .data = (uint8_t *)msg.blinding_factor.bytes,
+                .len = msg.blinding_factor.length
         };
-        
+
         if (bbs_create_proof_context_add_proof_message_bytes(handle, msgBuffer, msg.type, blindBuffer, &error) != 0) {
             bbs_byte_buffer_free(bbsPublicKeyBuffer);
             @throw [NSException exceptionWithName:@"BBSError"
-                                        reason:@"Unable to add proof message"
-                                      userInfo:nil];
+                                           reason:@"Unable to add proof message"
+                                         userInfo:nil];
         }
     }
-    
+
     // Finish and get proof
     bbs_signature_byte_buffer_t proofBuffer;
     if (bbs_create_proof_context_finish(handle, &proofBuffer, &error) != 0) {
         bbs_byte_buffer_free(bbsPublicKeyBuffer);
         @throw [NSException exceptionWithName:@"BBSError"
-                                    reason:@"Unable to create proof"
-                                  userInfo:nil];
+                                       reason:@"Unable to create proof"
+                                     userInfo:nil];
     }
-    
+
     // Convert to NSData and clean up
     NSData *proof = [NSData dataWithBytes:proofBuffer.data length:proofBuffer.len];
     bbs_byte_buffer_free(proofBuffer);
     bbs_byte_buffer_free(bbsPublicKeyBuffer);
-    
+
     return proof;
 }
 
@@ -154,123 +154,70 @@ static const int PROOF_MESSAGE_TYPE_HIDDEN_EXTERNAL_BLINDING = 3;
     @try {
         NSDictionary *args = call.arguments;
 
-        id publicKeyObject = args[@"publicKey"];
+        // Process required data fields
+        NSData *publicKey = [self processDataField:args[@"publicKey"] fieldName:@"publicKey"];
+        NSData *nonce = [self processDataField:args[@"nonce"] fieldName:@"nonce"];
+        NSData *signature = [self processDataField:args[@"signature"] fieldName:@"signature"];
 
-        NSData *publicKey = nil;
-
-        if ([publicKeyObject isKindOfClass:[FlutterStandardTypedData class]]) {
-            FlutterStandardTypedData *flutterData = (FlutterStandardTypedData *)publicKeyObject;
-            publicKey = flutterData.data; // Access the .data property
-            NSLog(@"Successfully extracted NSData from FlutterStandardTypedData. Length: %lu", (unsigned long)publicKey.length);
-        } else if ([publicKeyObject isKindOfClass:[NSArray class]]) {
-            // This is your OLD way, keep it if some calls might still send a list of numbers
-            NSLog(@"Warning: publicKey received as NSArray, converting using byteListToArray. This might be an older data format.");
-            publicKey = [self byteListToArray:(NSArray<NSNumber *> *)publicKeyObject];
-        } else if (publicKeyObject) {
-            // Handle other unexpected types or log an error
-            NSLog(@"Error: publicKey is of unexpected type: %@", [publicKeyObject class]);
-        } else {
-            NSLog(@"Error: publicKey is nil in args.");
-        }
-
-        id nonceObject = args[@"nonce"];
-
-        NSData *nonce = nil;
-
-        if ([nonceObject isKindOfClass:[FlutterStandardTypedData class]]) {
-            FlutterStandardTypedData *flutterData = (FlutterStandardTypedData *)nonceObject;
-            nonce = flutterData.data; // Access the .data property
-            NSLog(@"Successfully extracted NSData from FlutterStandardTypedData. Length: %lu", (unsigned long)nonce.length);
-        } else if ([nonceObject isKindOfClass:[NSArray class]]) {
-            // This is your OLD way, keep it if some calls might still send a list of numbers
-            NSLog(@"Warning: nonce received as NSArray, converting using byteListToArray. This might be an older data format.");
-            nonce = [self byteListToArray:(NSArray<NSNumber *> *)nonceObject];
-        } else if (nonceObject) {
-            // Handle other unexpected types or log an error
-            NSLog(@"Error: nonce is of unexpected type: %@", [nonceObject class]);
-        } else {
-            NSLog(@"Error: nonce is nil in args.");
-        }
-
-        id signatureObject = args[@"signature"];
-
-        NSData *signature = nil;
-
-        if ([signatureObject isKindOfClass:[FlutterStandardTypedData class]]) {
-            FlutterStandardTypedData *flutterData = (FlutterStandardTypedData *)signatureObject;
-            signature = flutterData.data; // Access the .data property
-            NSLog(@"Successfully extracted NSData from FlutterStandardTypedData. Length: %lu", (unsigned long)signature.length);
-        } else if ([signatureObject isKindOfClass:[NSArray class]]) {
-            // This is your OLD way, keep it if some calls might still send a list of numbers
-            NSLog(@"Warning: signature received as NSArray, converting using byteListToArray. This might be an older data format.");
-            signature = [self byteListToArray:(NSArray<NSNumber *> *)signatureObject];
-        } else if (signatureObject) {
-            // Handle other unexpected types or log an error
-            NSLog(@"Error: signature is of unexpected type: %@", [signatureObject class]);
-        } else {
-            NSLog(@"Error: signature is nil in args.");
-        }
-
+        // Process messages
         NSArray *msgList = args[@"messages"];
+        NSMutableArray *messages = [self processMessageList:msgList];
 
-        NSMutableArray *messages = [NSMutableArray arrayWithCapacity:msgList.count];
-        for (NSDictionary *msg in msgList) {
-
-            id messageObject = msg[@"message"];
-
-            NSData *message = nil;
-
-            if ([messageObject isKindOfClass:[FlutterStandardTypedData class]]) {
-                FlutterStandardTypedData *flutterData = (FlutterStandardTypedData *)messageObject;
-                message = flutterData.data; // Access the .data property
-                NSLog(@"Successfully extracted NSData from FlutterStandardTypedData. Length: %lu", (unsigned long)message.length);
-            } else if ([messageObject isKindOfClass:[NSArray class]]) {
-                // This is your OLD way, keep it if some calls might still send a list of numbers
-                NSLog(@"Warning: message received as NSArray, converting using byteListToArray. This might be an older data format.");
-                message = [self byteListToArray:(NSArray<NSNumber *> *)messageObject];
-            } else if (messageObject) {
-                // Handle other unexpected types or log an error
-                NSLog(@"Error: message is of unexpected type: %@", [messageObject class]);
-            } else {
-                NSLog(@"Error: message is nil in args.");
-            }
-
-            id blindingFactorObject = msg[@"blinding_factor"];
-
-            NSData *blindingFactor = nil;
-
-            if ([blindingFactorObject isKindOfClass:[FlutterStandardTypedData class]]) {
-                FlutterStandardTypedData *flutterData = (FlutterStandardTypedData *)blindingFactorObject;
-                blindingFactor = flutterData.data; // Access the .data property
-                NSLog(@"Successfully extracted NSData from FlutterStandardTypedData. Length: %lu", (unsigned long)blindingFactor.length);
-            } else if ([blindingFactorObject isKindOfClass:[NSArray class]]) {
-                // This is your OLD way, keep it if some calls might still send a list of numbers
-                NSLog(@"Warning: blindingFactor received as NSArray, converting using byteListToArray. This might be an older data format.");
-                blindingFactor = [self byteListToArray:(NSArray<NSNumber *> *)blindingFactorObject];
-            } else if (blindingFactorObject) {
-                // Handle other unexpected types or log an error
-                NSLog(@"Error: blindingFactor is of unexpected type: %@", [blindingFactorObject class]);
-            } else {
-                NSLog(@"Error: blindingFactor is nil in args.");
-            }
-
-            ProofMessage *proofMsg = [[ProofMessage alloc] initWithType:[msg[@"type"] intValue]
-                                                              message:message
-                                                             blinding:blindingFactor];
-            [messages addObject:proofMsg];
-        }
-
+        // Create proof and return result
         NSData *proof = [self blsCreateProof:publicKey
-                                     nonce:nonce
-                                signature:signature
-                                 messages:messages];
+                                       nonce:nonce
+                                   signature:signature
+                                    messages:messages];
         result([self toList:proof]);
     } @catch (NSException *exception) {
         result([FlutterError errorWithCode:@"BLS_CREATE_PROOF_FAILED"
-                                 message:exception.reason
-                                 details:nil]);
+                                   message:exception.reason
+                                   details:nil]);
     }
 }
+
+// Helper method to process data fields
+- (NSData *)processDataField:(id)dataObject fieldName:(NSString *)fieldName {
+    NSData *processedData = nil;
+
+    if ([dataObject isKindOfClass:[FlutterStandardTypedData class]]) {
+        FlutterStandardTypedData *flutterData = (FlutterStandardTypedData *)dataObject;
+        processedData = flutterData.data;
+        NSLog(@"Successfully extracted NSData from FlutterStandardTypedData. Length: %lu", (unsigned long)processedData.length);
+    } else if ([dataObject isKindOfClass:[NSArray class]]) {
+        NSLog(@"Warning: %@ received as NSArray, converting using byteListToArray. This might be an older data format.", fieldName);
+        processedData = [self byteListToArray:(NSArray<NSNumber *> *)dataObject];
+    } else if (dataObject) {
+        [self logError:fieldName type:[dataObject class]];
+    } else {
+        NSLog(@"Error: %@ is nil in args.", fieldName);
+    }
+
+    return processedData;
+}
+
+// Helper method to process message list
+- (NSMutableArray *)processMessageList:(NSArray *)msgList {
+    NSMutableArray *messages = [NSMutableArray arrayWithCapacity:msgList.count];
+
+    for (NSDictionary *msg in msgList) {
+        NSData *message = [self processDataField:msg[@"message"] fieldName:@"message"];
+        NSData *blindingFactor = [self processDataField:msg[@"blinding_factor"] fieldName:@"blinding_factor"];
+
+        ProofMessage *proofMsg = [[ProofMessage alloc] initWithType:[msg[@"type"] intValue]
+                                                            message:message
+                                                           blinding:blindingFactor];
+        [messages addObject:proofMsg];
+    }
+
+    return messages;
+}
+
+// Helper method for error logging
+- (void)logError:(NSString *)fieldName type:(Class)class {
+    NSLog(@"Error: %@ is of unexpected type: %@", fieldName, class);
+}
+
 
 #pragma mark - Helper Methods
 
--- ios/zetrix_vc_flutter.podspec
+++ ios/zetrix_vc_flutter.podspec
@@ -1,35 +1,32 @@
 Pod::Spec.new do |spec|
-  spec.name          = 'zetrix_vc_flutter'
-  spec.version       = '1.0.0'
-  spec.summary       = 'A Flutter plugin for platform-specific Zetrix VC(VP Generation) functionality with BBS Signature.'
-  spec.description   = 'This plugin adds platform-specific functionality to Zetrix VC(VP Generation)  on iOS.'
-  spec.homepage      = 'https://example.com/zetrix'
-  spec.source        = { :git => 'https://github.com/Zetrix-Chain/zetrix-vc-flutter.git' }
-  spec.license       = { :type => 'MIT', :file => 'LICENSE' }
-  spec.author        = { 'Author Name' => 'email@example.com' }
-  spec.ios.deployment_target = '12.0'
+    spec.name          = 'zetrix_vc_flutter'
+    spec.version       = '1.0.0'
+    spec.summary       = 'A Flutter plugin for platform-specific Zetrix VC(VP Generation) functionality with BBS Signature.'
+    spec.description   = 'This plugin adds platform-specific functionality to Zetrix VC(VP Generation)  on iOS.'
+    spec.homepage      = 'https://example.com/zetrix'
+    spec.source        = { :git => 'https://github.com/Zetrix-Chain/zetrix-vc-flutter.git' }
+    spec.license       = { :type => 'MIT', :file => 'LICENSE' }
+    spec.author        = { 'Author Name' => 'email@example.com' }
+    spec.ios.deployment_target = '12.0'
 
-  # Source files including private headers
-  spec.source_files = 'Classes/**/*.{h,m}'
+    # Source files including private headers
+    spec.source_files = 'Classes/**/*.{h,m}'
 
-  # Public headers that should be exposed to the plugin
-  spec.public_header_files = 'Classes/**/*.h'
+    # Public headers that should be exposed to the plugin
+    spec.public_header_files = 'Classes/**/*.h'
 
-  # Static libraries
-  spec.vendored_libraries = 'Frameworks/libbbs.a'
+    # Static libraries
+    spec.vendored_libraries = 'Frameworks/libbbs.a'
 
-  # Headers from Frameworks directory
-  spec.preserve_paths = 'Frameworks/**/*.h'
+    # Add the correct header search paths
+    spec.pod_target_xcconfig = {
+        'HEADER_SEARCH_PATHS' => [
+          '$(PODS_TARGET_SRCROOT)/Classes'
+        ],
+        'LIBRARY_SEARCH_PATHS' => '$(PODS_TARGET_SRCROOT)/Frameworks',
+    }
 
-
-  # Add the correct header search paths
-  spec.pod_target_xcconfig = {
-      'HEADER_SEARCH_PATHS' => [
-        '$(PODS_TARGET_SRCROOT)/Classes',
-        '$(PODS_TARGET_SRCROOT)/Frameworks'
-      ]
-  }
-
-  spec.dependency 'Flutter'
-  # Add additional resources if required by your `.xcodeproj` or static library
+    spec.dependency 'Flutter'
+    # Add additional resources if required by your `.xcodeproj` or static library
 end
+
